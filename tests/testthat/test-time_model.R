bbe534277cc965b83fcf7d1658d4fd69e57ade7f
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-01-18 15:05:44 +0100

    explicit argument names added and covariate tests
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- tests/testthat/test-time_model.R
+++ tests/testthat/test-time_model.R
@@ -1,6 +1,6 @@
 test_that("Cubic slope", {
 
-  x <- time_model("age", "log(bmi)", bmigrowth[bmigrowth$sex == 0, ], "cubic_slope")
+  x <- time_model(x = "age", y = "log(bmi)", data = bmigrowth[bmigrowth$sex == 0, ], method = "cubic_slope")
 
   y <- nlme::lme(
     log(bmi) ~ stats::poly(age, 3),
@@ -20,7 +20,7 @@ test_that("Cubic slope", {
 
 test_that("Linear Splines", {
 
-  x <- time_model("age", "log(bmi)", bmigrowth[bmigrowth$sex == 0, ], "linear_splines")
+  x <- time_model(x = "age", y = "log(bmi)", data = bmigrowth[bmigrowth$sex == 0, ], method = "linear_splines")
 
   y <- nlme::lme(
     log(bmi) ~ gsp(age, knots = c(5.5, 11), degree = c(1, 1, 1), smooth = c(0, 0)),
@@ -40,7 +40,7 @@ test_that("Linear Splines", {
 
 test_that("Cubic Splines", {
 
-  x <- time_model("age", "log(bmi)", bmigrowth[bmigrowth$sex == 0, ], "cubic_splines")
+  x <- time_model(x = "age", y = "log(bmi)", data = bmigrowth[bmigrowth$sex == 0, ], method = "cubic_splines")
 
   y <- nlme::lme(
     log(bmi) ~ gsp(age, knots = c(2, 8, 12), degree = c(3, 3, 3, 3), smooth = c(2, 2, 2)),
@@ -56,4 +56,24 @@ test_that("Cubic Splines", {
   y <- stats::coefficients(summary(y))
 
   expect_equivalent(x, y)
+})
+
+test_that("Test wrong covariates", {
+  expect_error(
+    object = time_model(x = "age", y = "log(bmi)", cov = c("nothing"), data = bmigrowth[bmigrowth$sex == 0, ], method = "linear_splines")
+  )
+})
+
+test_that("Test covariates", {
+  expect_s3_class(
+    object = time_model(x = "age", y = "log(bmi)", cov = c("height"), data = bmigrowth[bmigrowth$sex == 0, ], method = "linear_splines"),
+    class = "lme"
+  )
+})
+
+test_that("Test covariates with trnasformation", {
+  expect_s3_class(
+    object = time_model(x = "age", y = "log(bmi)", cov = c("log(height)"), data = bmigrowth[bmigrowth$sex == 0, ], method = "linear_splines"),
+    class = "lme"
+  )
 })
