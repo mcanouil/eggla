c7e474fe4dcf0fd413ad4d19f4b0f99d14caeffb
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-10-12 13:08:39 +0200

    little refactoring; fix lint notes

--- data-raw/bmigrowth.R
+++ data-raw/bmigrowth.R
@@ -1,3 +1,3 @@
 bmigrowth <- read.csv("data-raw/bmigrowth.csv.gz")
-bmigrowth[["ID"]] <- sprintf("%03d", bmigrowth[["ID"]] )
+bmigrowth[["ID"]] <- sprintf("%03d", bmigrowth[["ID"]])
 usethis::use_data(bmigrowth, overwrite = TRUE)
