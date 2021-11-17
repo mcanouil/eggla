b811d5562f461579c89c04476edc068556f6ef41
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-11-17 15:39:02 +0100

    mv to allow acces via system.file

--- inst/setup/setup.R
+++ inst/setup/setup.R
@@ -1,6 +1,20 @@
 #!/usr/bin/env Rscript
 
 parent_directory <- "/tmp"
+data_path <- ""
+data <- fread(data_path)[
+  j = `:=`(
+    "agedays" = floor(age * 365.25), # convert age in years to age in days and as integers ...
+    "WEIGHTKG" = as.numeric(weight),
+    "HEIGHTCM" = as.numeric(height)
+  )
+][
+  j = `:=`(# recode sex with Male = 0 and Female = 1 ...
+    "sex_daymont" = c("0" = "1", "1" = "0")[as.character(sex)]
+    # "sex_daymont" = as.character(sex) # if already coded with Male = 0 and Female = 1
+  )
+]
+
 project_directory <- file.path(parent_directory, "egg_analysis")
 dir.create(project_directory, recursive = TRUE)
 setwd(project_directory)
