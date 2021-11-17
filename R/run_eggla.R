d06bdf4f96a937945709c619b84395501fb971b2
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-11-17 17:36:40 +0100

    one function for all

--- R/run_eggla.R
+++ R/run_eggla.R
@@ -1,58 +1,178 @@
 #' run_eggla
 #'
-#' @param phenotypes something
-#' @param trait something
-#' @param covariates something
-#' @param daymont_qc something
-#' @param daymont_flags_exclude something
-#' @param cohort_name  something
-#' @param output_directory something
-#' @param ... Parameters to pass to `rmarkdown::render()`.
+#' Perform Daymont's quality-control for BMI, 
+#' fit a cubic splines mixed model regression with linear splines as random effect,
+#' save model object, generates residuals figures fot model validity,
+#' derived area under the curve and slopes for male and femal.
 #'
-#' @return NULL
-#' @keywords internal
+#' @param data something
+#' @param id_variable something
+#' @param age_days_variable something
+#' @param age_years_variable something
+#' @param weight_kilograms_variable something
+#' @param height_centimetres_variable something
+#' @param sex_variable something
+#' @param covariates something
+#' @param male_coded_zero something
+#' @param parallel something
+#' @param parallel_n_chunks something
+#' @param working_directory something
+#'
+#' @return Path to zip archives
+#' @import data.table
+#'
+#' @export
 run_eggla <- function(
-  phenotypes = NULL,
-  trait = "bmi",
-  covariates = NULL,
-  daymont_qc = TRUE,
-  daymont_flags_exclude = TRUE,
-  cohort_name = "cohort",
-  output_directory = tempdir(),
-  ...
+  data,
+  id_variable,
+  age_days_variable,
+  age_years_variable,
+  weight_kilograms_variable,
+  height_centimetres_variable,
+  sex_variable,
+  covariates,
+  male_coded_zero = FALSE,
+  parallel = FALSE,
+  parallel_n_chunks = 1,
+  working_directory = getwd()
 ) {
-  if (is.null(phenotypes) || (!grepl("\\.csv", phenotypes) && !inherits(x = phenotypes, what = "data.frame"))) {
-    stop('"phenotypes" must be either a path to a csv file or inherits from "data.frame" class.')
-  }
+  HEIGHTCM <- WEIGHTKG <- bmi <- clean <- NULL
+  egg_agedays <- egg_id <- egg_sex <- NULL
+  measurement <- param <- NULL
 
-  invisible(file.copy(
-    from = system.file("rmarkdown", "templates", "eggla", "skeleton", "skeleton.Rmd", package = "eggla"),
-    to = file.path(output_directory, "eggla.Rmd"),
-    overwrite = TRUE
-  ))
-
-  if (!all(sapply(
-    X = strsplit(gsub("\\([^()]+\\)|\n| ", "", utils::packageDescription("eggla", fields = "Suggests")), ",")[[1]],
-    FUN = function(x) nchar(system.file(package = x)) > 0
-  ))) {
-    stop("Packages listed in 'Suggests' field are needed to run this function.")
-  }
-
-  rmarkdown::render(
-    input = file.path(output_directory, "eggla.Rmd"),
-    # output_file = output_file,
-    # output_dir = output_directory,
-    params = list(
-      cohort_name =  cohort_name,
-      phenotypes = phenotypes,
-      trait = trait,
-      covariates = covariates,
-      daymont_qc = daymont_qc,
-      daymont_flags_exclude = daymont_flags_exclude,
-      output_directory = output_directory
-    ),
-    ...
+  dt_long <- data.table::melt(
+    data = data.table::as.data.table(data)[
+      j = list(
+        "egg_id" = get(id_variable),
+        "egg_agedays" = if (is.null(age_days_variable)) {
+          floor(get(age_years_variable) * 365.25) # convert age in years to age in days and as integers ...
+        } else {
+          get(age_days_variable)
+        },
+        "WEIGHTKG" = as.numeric(get(weight_kilograms_variable)),
+        "HEIGHTCM" = as.numeric(get(height_centimetres_variable)),
+        "egg_sex" = if (male_coded_zero) {
+          as.character(get(sex_variable))
+        } else {
+          c("0" = "1", "1" = "0")[as.character(get(sex_variable))] # recode sex with Male = 0 and Female = 1 ...
+        }
+      )
+    ],
+    id.vars = c(sprintf("egg_%s", c("id", "agedays", "sex")), covariates),
+    measure.vars = c("WEIGHTKG", "HEIGHTCM"),
+    variable.name = "param",
+    value.name = "measurement"
   )
 
-  invisible()
+  dt_long[
+    j = `:=`(
+      "clean" = growthcleanr::cleangrowth(
+        subjid = egg_id,
+        param = param,
+        agedays = egg_agedays,
+        sex = egg_sex,
+        measurement = measurement,
+        quietly = TRUE,
+        parallel = parallel,
+        num.batches = parallel_n_chunks
+      )
+    )
+  ]
+
+  dt_clean <- data.table::dcast(
+    data = dt_long[clean %in% "Include"], # Exclude all flags
+    formula = ... ~ param,
+    value.var = "measurement"
+  )[
+    j = `:=`("bmi" = WEIGHTKG / (HEIGHTCM / 100)^2) # recompute bmi based using QCed variables
+  ][
+    !is.na(bmi) # exclude missing BMI related to measurements exclusion
+  ]
+
+  base_model <- log(bmi) ~ egg_agedays
+  if (!is.null(covariates)) {
+    base_model <- stats::update(
+      base_model,
+      stats::as.formula(sprintf(". ~ . + %s", paste(covariates, collapse = " + ")))
+    )
+  }
+
+  archives <- sapply(
+    X = c(0, 1),
+    FUN = function(isex) {
+      sex_literal <- c("0" = "male", "1" = "female")[as.character(isex)]
+      results_directory <- file.path(working_directory, sex_literal)
+      dir.create(results_directory, recursive = TRUE)
+      on.exit(unlink(results_directory, recursive = TRUE))
+      results <- egg_model(
+        formula = base_model,
+        data = dt_clean[egg_sex %in% isex],
+        id_var = "egg_id"
+      )
+
+      saveRDS(
+        object = results,
+        file = file.path(results_directory, "model-object.rds")
+      )
+
+      data.table::fwrite(
+        x = broom.mixed::tidy(results),
+        file = file.path(results_directory, "model-coefficients.csv")
+      )
+
+      data.table::fwrite(
+        x = egg_slopes(
+          fit = results,
+          period = c(0, 0.5, 1.5, 5, 6, 10, 12, 17)
+        ),
+        file = file.path(results_directory, "derived-slopes.csv")
+      )
+
+      data.table::fwrite(
+        x = egg_auc(
+          fit = results,
+          period = c(0, 0.5, 1.5, 5, 6, 10, 12, 17)
+        ),
+        file = file.path(results_directory, "derived-auc.csv")
+      )
+
+      svglite::svglite(
+        filename = file.path(results_directory, "model-residuals.svg"),
+        width = 10,
+        height = 8
+      )
+      print(
+        plot_residuals(
+          x = "age",
+          y = "log(bmi)",
+          fit = results
+        ) +
+          patchwork::plot_annotation(
+            title = sprintf(
+              "Cubic Splines (Random Linear Splines) - BMI - %s",
+              c("0" = "Male", "1" = "Female")[as.character(isex)]
+            ),
+            tag_levels = "A"
+          )
+      )
+      invisible(grDevices::dev.off())
+
+      owd <- getwd()
+      on.exit(setwd(owd), add = TRUE)
+      setwd(results_directory)
+      archive_filename <- file.path(
+        working_directory,
+        sprintf("%s-%s.zip", Sys.Date(), sex_literal)
+      )
+      utils::zip(
+        zipfile = archive_filename,
+        files = list.files()
+      )
+      archive_filename
+    }
+  )
+
+  message("Results available at:")
+  message(paste(sprintf("+ '%s'", archives), collapse = "\n"))
+  archives
 }
--- inst/setup/analysis.R
+++ inst/setup/analysis.R
@@ -5,157 +5,17 @@ parent_directory <- getwd()
 setwd(file.path(parent_directory, "egg_analysis"))
 renv::activate()
 
-# Setup
-data_path <- ""
-id_variable <- "ID"
-age_days_variable <- NULL
-age_years_variable <- "age"
-weight_kilograms_variable <- "weight"
-height_centimetres_variable <- "height"
-sex_variable <- "sex"
-covariates <- NULL # or c("cov1", "cov2")
-male_coded_zero <- FALSE
-parallel <- FALSE
-parallel_n_chunks <- 1
-
-library(data.table)
-library(eggla)
-library(growthcleanr)
-library(broom.mixed)
-library(patchwork)
-
-dt <- fread(data_path)
-
-# Data formatting
-dt_long <- melt(
-  data = dt[
-    j = list(
-      "egg_id" = get(id_variable),
-      "egg_agedays" = if (is.null(age_days_variable)) {
-        floor(get(age_years_variable) * 365.25) # convert age in years to age in days and as integers ...
-      } else {
-        get(age_days_variable)
-      },
-      "WEIGHTKG" = as.numeric(get(weight_kilograms_variable)),
-      "HEIGHTCM" = as.numeric(get(height_centimetres_variable)),
-      "egg_sex" = if (male_coded_zero) {
-        as.character(get(sex_variable))
-      } else {
-        c("0" = "1", "1" = "0")[as.character(get(sex_variable))] # recode sex with Male = 0 and Female = 1 ...
-      }
-    )
-  ],
-  id.vars = c(sprintf("egg_%s", c("id", "agedays", "sex")), covariates),
-  measure.vars = c("WEIGHTKG", "HEIGHTCM"),
-  variable.name = "param",
-  value.name = "measurement"
+eggla::run_eggla(
+  data = data.table::fread("data_file_path"),
+  id_variable = "ID",
+  age_days_variable = NULL,
+  age_years_variable = "age",
+  weight_kilograms_variable = "weight",
+  height_centimetres_variable = "height",
+  sex_variable = "sex",
+  covariates = NULL,
+  male_coded_zero = FALSE,
+  parallel = FALSE,
+  parallel_n_chunks = 1,
+  working_directory = parent_directory
 )
-
-dt_long[
-  j = clean := cleangrowth( # Daymont's QC from 'growthcleanr'
-    subjid = egg_id,
-    param = param,
-    agedays = egg_agedays,
-    sex = egg_sex,
-    measurement = measurement,
-    quietly = TRUE,
-    parallel = parallel,
-    num.batches = parallel_n_chunks
-  )
-]
-
-dt_clean <- dcast(
-  data = dt_long[clean %in% "Include"], # Exclude all flags
-  formula = ... ~ param,
-  value.var = "measurement"
-)[
-  j = "bmi" := WEIGHTKG / (HEIGHTCM / 100)^2 # recompute bmi based using QCed variables
-][
-  !is.na(bmi) # exclude missing BMI related to measurements exclusion
-]
-
-base_model <- log(bmi) ~ egg_agedays
-if (!is.null(covariates)) {
-  base_model <- update(
-    base_model,
-    as.formula(sprintf(". ~ . + %s", paste(covariates, collapse = " + ")))
-  )
-}
-
-archives <- sapply(
-  X = c(0, 1),
-  FUN = function(isex) {
-    sex_literal <- c("0" = "male", "1" = "female")[as.character(isex)]
-    results_directory <- file.path(parent_directory, sex_literal)
-    dir.create(results_directory, recursive = TRUE)
-    on.exit(unlink(results_directory, recursive = TRUE))
-    results <- egg_model(
-      formula = base_model,
-      data = dt_clean[egg_sex %in% isex],
-      id_var = "egg_id"
-    )
-
-    saveRDS(
-      object = results,
-      file = file.path(results_directory, "model-object.rds")
-    )
-
-    fwrite(
-      x = tidy(results),
-      file = file.path(results_directory, "model-coefficients.csv")
-    )
-
-    fwrite(
-      x = egg_slopes(
-        fit = res,
-        period = c(0, 0.5, 1.5, 5, 6, 10, 12, 17)
-      ),
-      file = file.path(results_directory, "derived-slopes.csv")
-    )
-
-    fwrite(
-      x = egg_auc(
-        fit = res,
-        period = c(0, 0.5, 1.5, 5, 6, 10, 12, 17)
-      ),
-      file = file.path(results_directory, "derived-auc.csv")
-    )
-
-    svglite(
-      filename = file.path(results_directory, "model-residuals.svg"),
-      width = 10,
-      height = 8
-    )
-    print(
-      plot_residuals(
-        x = "age",
-        y = "log(bmi)",
-        fit = res
-      ) +
-        plot_annotation(
-          title = sprintf(
-            "Cubic Splines (Random Linear Splines) - BMI - %s",
-            c("0" = "Male", "1" = "Female")[as.character(isex)]
-          ),
-          tag_levels = "A"
-        )
-    )
-    invisible(dev.off())
-
-    owd <- getwd()
-    on.exit(setwd(owd), add = TRUE)
-    setwd(results_directory)
-    archive_filename <- file.path(
-      parent_directory,
-      sprintf("%s-%s.zip", Sys.Date(), sex_literal)
-    )
-    zip(
-      zipfile = archive_filename,
-      files = list.files()
-    )
-    archive_filename
-  }
-)
-
-message("Results available at:")
-message(paste(sprintf("+ '%s'", archives), collapse = "\n"))
