9ef11d4a77d6366627b1039ed5385e90a17b6683
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-11-17 16:58:14 +0100

    only setup renv project

--- inst/setup/setup.R
+++ inst/setup/setup.R
@@ -1,19 +1,6 @@
 #!/usr/bin/env Rscript
 
-parent_directory <- "/tmp"
-data_path <- ""
-data <- fread(data_path)[
-  j = `:=`(
-    "agedays" = floor(age * 365.25), # convert age in years to age in days and as integers ...
-    "WEIGHTKG" = as.numeric(weight),
-    "HEIGHTCM" = as.numeric(height)
-  )
-][
-  j = `:=`(# recode sex with Male = 0 and Female = 1 ...
-    "sex_daymont" = c("0" = "1", "1" = "0")[as.character(sex)]
-    # "sex_daymont" = as.character(sex) # if already coded with Male = 0 and Female = 1
-  )
-]
+parent_directory <- getwd()
 
 project_directory <- file.path(parent_directory, "egg_analysis")
 dir.create(project_directory, recursive = TRUE)
@@ -24,4 +11,5 @@ install.packages("renv", lib = temp_library, repos = "http://cloud.r-project.org
 library("renv", lib.loc = temp_library)
 init()
 install("mcanouil/eggla@v0.3.0")
-restore(lockfile = system.file("renv.lock", package = "eggla"))
+restore(lockfile = system.file("setup", "renv.lock", package = "eggla"))
+renv::snapshot(force = TRUE)
