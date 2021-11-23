b4044b1909100d68f51b5a2439041d3bede7d229
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-11-23 15:51:10 +0100

    update renv.lock for ragg

--- data-raw/renv.lock.R
+++ data-raw/renv.lock.R
@@ -1,19 +1,4 @@
-# apply(
-#   X = desc::description$new()$get_deps()[-1, ],
-#   MARGIN = 1,
-#   FUN = function(row) {
-#     ver <- sub("\\*|>= ", "", row[3])
-#     if (nchar(ver) > 0) {
-#       paste0(row[2], "@", ver)
-#     } else {
-#       row[2]
-#     }
-#   }
-# )
 options("repos" = list(CRAN = "https://cran.r-project.org"))
-renv::snapshot(
-  packages = subset(desc::description$new()$get_deps(), type %in% "Imports")[["package"]],
-  lockfile = "inst/setup/renv.lock",
-  prompt  = FALSE,
-  force = TRUE
-)
+renv::init(bare = TRUE, force = TRUE)
+renv::install(".", prompt = FALSE)
+renv::snapshot(lockfile = "inst/setup/renv.lock", type = "explicit", force = TRUE)
