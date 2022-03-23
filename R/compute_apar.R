b92752fea78b0c04b217f998238f9cb2683d1a89
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-03-23 20:21:25 +0100

    fix: ensure object type or error out

--- R/compute_apar.R
+++ R/compute_apar.R
@@ -27,36 +27,37 @@
 #'
 #' head(res_apar[AP | AR])
 compute_apar <- function(fit, id_var, age_var) {
-    bmi_pred <- egg_ageyears <- log_bmi_pred <- NULL
-    out <- data.table::setnames(
-      x = data.table::data.table(
-        egg_id = unique(fit[["groups"]][[id_var]]),
-        egg_ageyears = list(seq(from = 0.25, to = 10, by = 0.05))
-      ),
-      old = c("egg_id", "egg_ageyears"),
-      new = c(id_var, age_var)
-    )[
-      j = `names<-`(list(unlist(.SD)), age_var),
-      .SDcols = c(age_var),
-      by = c(id_var)
-    ][
-      j = log_bmi_pred := stats::predict(
-        object = fit,
-        newdata = .SD,
-        interval = "prediction"
-      )
-    ][
-      j = bmi_pred := exp(log_bmi_pred)
-    ]
-    data.table::setnames(
-      x = out,
-      old = c(id_var, age_var),
-      new = c("egg_id", "egg_ageyears")
-    )[
-      j = `:=`(
-        AP = egg_ageyears %in% egg_ageyears[which(diff(sign(diff(bmi_pred))) == -2) + 1],
-        AR = egg_ageyears %in% egg_ageyears[which(diff(sign(diff(bmi_pred))) == +2) + 1]
-      ),
-      by = "egg_id"
-    ]
-  }
+  if (!inherits(lme, "lmeObject")) stop("\"fit\" must be a \"lmeObject\"!")
+  bmi_pred <- egg_ageyears <- log_bmi_pred <- NULL
+  out <- data.table::setnames(
+    x = data.table::data.table(
+      egg_id = unique(fit[["groups"]][[id_var]]),
+      egg_ageyears = list(seq(from = 0.25, to = 10, by = 0.05))
+    ),
+    old = c("egg_id", "egg_ageyears"),
+    new = c(id_var, age_var)
+  )[
+    j = `names<-`(list(unlist(.SD)), age_var),
+    .SDcols = c(age_var),
+    by = c(id_var)
+  ][
+    j = log_bmi_pred := stats::predict(
+      object = fit,
+      newdata = .SD,
+      interval = "prediction"
+    )
+  ][
+    j = bmi_pred := exp(log_bmi_pred)
+  ]
+  data.table::setnames(
+    x = out,
+    old = c(id_var, age_var),
+    new = c("egg_id", "egg_ageyears")
+  )[
+    j = `:=`(
+      AP = egg_ageyears %in% egg_ageyears[which(diff(sign(diff(bmi_pred))) == -2) + 1],
+      AR = egg_ageyears %in% egg_ageyears[which(diff(sign(diff(bmi_pred))) == +2) + 1]
+    ),
+    by = "egg_id"
+  ]
+}
