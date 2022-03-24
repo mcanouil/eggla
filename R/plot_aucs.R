660cb59412718858c2d8b0cd5ba015aa41627fbe
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-03-24 13:06:04 +0100

    chore: add function skeletons

---
+++ R/plot_aucs.R
@@ -0,0 +1,13 @@
+#' Plot derived area under the curves from a model fitted by `time_model` or `egg_model`.
+#'
+#' @param fit A model object from a statistical model
+#'   such as from a call `nlme::lme()`, `time_model()` or `egg_model`.
+#'
+#' @return A `patchwork` `ggplot2` object.
+#'
+#' @export
+#'
+#' @examples
+plot_aucs <- function(fit) {
+  
+}
