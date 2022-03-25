dc1833c26ad863294c347e63d92ee05edcdabce9
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-03-25 10:53:57 +0100

    chore: update/upgrade lock files

--- data-raw/renv.lock.R
+++ data-raw/renv.lock.R
@@ -1,5 +1,4 @@
 renv::init(bare = TRUE, force = TRUE)
-renv::snapshot(lockfile = "inst/setup/renv.lock", packages = ".", force = TRUE)
+renv::snapshot(lockfile = "inst/setup/renv.lock", packages = ".", force = TRUE, prompt = FALSE)
 unlink(".Rprofile")
-unlink("renv", recursive = TRUE)
-renv::record("renv@0.15.4", lockfile = "inst/setup/renv.lock")
+unlink(list.files("renv", include.dirs = TRUE, all.files = TRUE, no.. = TRUE, full.names = TRUE), recursive = TRUE)
