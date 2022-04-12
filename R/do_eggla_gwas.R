dd049a32698d7c202e3112dafc1f4af113d3b683
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-04-12 13:15:51 +0200

    fix: slope file not deleted

--- R/do_eggla_gwas.R
+++ R/do_eggla_gwas.R
@@ -150,7 +150,7 @@ do_eggla_gwas <- function(
             files = "derived-aucs.csv",
             exdir = path
           )
-          on.exit(unlink(file.path(path, "derived-aucs.csv")))
+          on.exit(unlink(file.path(path, c("derived-slopes.csv", "derived-aucs.csv"))))
           data.table::merge.data.table(
             x = data.table::fread(file.path(path, "derived-slopes.csv")),
             y = data.table::fread(file.path(path, "derived-aucs.csv")),
