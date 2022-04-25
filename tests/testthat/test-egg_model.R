58bff818d63fe73658aa7ae8b25b103c952c0e7e
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-04-25 14:46:56 +0200

    feat: allows to use (or not) AR(1)
    if TRUE, try without if all models failed

---
+++ tests/testthat/test-egg_model.R
@@ -0,0 +1,16 @@
+data("bmigrowth")
+
+test_that("egg_model", {
+  for (i in c(FALSE, TRUE)) {
+    expect_s3_class(
+      egg_model(
+        formula = log(bmi) ~ age,
+        data = bmigrowth[bmigrowth[["sex"]] == 0, ],
+        id_var = "ID",
+        random_complexity = 1,
+        use_ar1 = TRUE
+      ),
+      "lme"
+    )
+  }
+})
