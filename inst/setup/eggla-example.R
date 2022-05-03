fbe8f3e74cfb7637641f837f0cd5d94fc9c0ebdf
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-05-03 14:12:08 +0200

    feat: add example script to test Docker image

---
+++ inst/setup/eggla-example.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+
+args <- commandArgs(trailingOnly = TRUE)
+
+library(eggla)
+
+data("bmigrowth")
+run_eggla(
+  data = bmigrowth,
+  id_variable = "ID",
+  age_days_variable = NULL,
+  age_years_variable = "age",
+  weight_kilograms_variable = "weight",
+  height_centimetres_variable = "height",
+  sex_variable = "sex",
+  covariates = NULL,
+  male_coded_zero = FALSE,
+  random_complexity = "auto",
+  use_ar1 = TRUE,
+  parallel = FALSE, # to parallelise Daymont QC
+  parallel_n_chunks = 1, # to parallelise Daymont QC
+  working_directory = tempdir()
+)
