88cfea8780af4f627d3b2939fcd8d6fe653fdd9d
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-05-18 12:16:27 +0200

    refactor: run model once and quietly

--- tests/testthat/test-compute_apar.R
+++ tests/testthat/test-compute_apar.R
@@ -1,12 +1,15 @@
 test_that("Adiposity Peak & Rebound", {
   for (i in c("predicted", "observed")) {
-    data("bmigrowth")
-    res <- egg_model(
-      formula = log(bmi) ~ age,
-      data = bmigrowth[bmigrowth[["sex"]] == 0, ],
-      id_var = "ID",
-      random_complexity = 1
-    )
+  data("bmigrowth")
+  res <- egg_model(
+    formula = log(bmi) ~ age,
+    data = bmigrowth[bmigrowth[["sex"]] == 0, ],
+    id_var = "ID",
+    random_complexity = 1,
+    use_ar1 = TRUE,
+    quiet = TRUE
+  )
+  for (i in c("predicted", "observed")) {
     expect_snapshot(compute_apar(fit = res, from = i)[AP | AR])
   }
 })
