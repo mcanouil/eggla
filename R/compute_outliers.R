e192e70f3b8add570f7ea9478618a81d99a7ea3d
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-07-27 11:38:13 +0200

    feat: draft function skeleton

---
+++ R/compute_outliers.R
@@ -0,0 +1,36 @@
+#' Compute outliers detection in AUCs/Slopes derived parameters.
+#'
+#' Based on computed area under the curves (_i.e._, `compute_aucs()`)
+#' and slopes (_i.e._, `compute_slopes()`) for several intervals using
+#' a model fitted by `time_model()`, compute an outlier detection for
+#' each intervals derived parameters.
+#'
+#' @param dt A `data.frame` with AUC or slopes for each individuals/samples.
+#'
+#' @return A `data.frame` with the individuals and measures flagged as outliers.
+#'
+#' @export
+#'
+#' @examples
+#' data("bmigrowth")
+#' ls_mod <- time_model(
+#'   x = "age",
+#'   y = "log(bmi)",
+#'   cov = NULL,
+#'   data = bmigrowth[bmigrowth[["sex"]] == 0, ],
+#'   method = "linear_splines"
+#' )
+#' aucs <- compute_aucs(
+#'   fit = ls_mod,
+#'   method = "linear_splines",
+#'   period = c(0, 0.5, 1.5, 3.5, 6.5, 10, 12, 17)#,
+#'   # knots = list(
+#'   #   "cubic_slope" = NULL,
+#'   #   "linear_splines" = c(5.5, 11),
+#'   #   "cubic_splines" = c(2, 8, 12)
+#'   # )[[method]]
+#' )
+#' compute_outliers(aucs)
+compute_outliers <- function(data) {
+
+}
