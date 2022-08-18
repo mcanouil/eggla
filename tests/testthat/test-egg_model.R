7132f1fd1f922f5459224fa7fef71dc79ab32a5d
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-08-18 15:49:20 +0200

    test: move run_eggla function to test file "run_eggla"

--- tests/testthat/test-egg_model.R
+++ tests/testthat/test-egg_model.R
@@ -49,24 +49,4 @@ test_that("egg_model", {
       knots = c(2, 8, 12)
     )
   )
-
-  expect_equal(
-    sort(basename(run_eggla_lmm(
-      data = bmigrowth,
-      id_variable = "ID",
-      age_days_variable = NULL,
-      age_years_variable = "age",
-      weight_kilograms_variable = "weight",
-      height_centimetres_variable = "height",
-      sex_variable = "sex",
-      covariates = NULL,
-      male_coded_zero = FALSE,
-      random_complexity = "auto", # simple to speed-up test
-      use_car1 = TRUE,
-      parallel = FALSE, # to parallelise Daymont QC
-      parallel_n_chunks = 1, # to parallelise Daymont QC
-      working_directory = tempdir()
-    ))),
-    sort(c("female.zip", "male.zip"))
-  )
 })
