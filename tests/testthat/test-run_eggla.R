6dd8b7623e81ea74a9e8186b5ae6c863d784487c
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-08-18 15:49:38 +0200

    test: new tests for run_eggla functions

---
+++ tests/testthat/test-run_eggla.R
@@ -0,0 +1,63 @@
+set.seed(2705)
+
+data("bmigrowth")
+
+test_that("run_eggla", {
+  result_lmm <- run_eggla_lmm(
+    data = bmigrowth,
+    id_variable = "ID",
+    age_days_variable = NULL,
+    age_years_variable = "age",
+    weight_kilograms_variable = "weight",
+    height_centimetres_variable = "height",
+    sex_variable = "sex",
+    covariates = NULL,
+    male_coded_zero = FALSE,
+    random_complexity = "auto", # simple to speed-up test
+    use_car1 = TRUE,
+    parallel = FALSE, # to parallelise Daymont QC
+    parallel_n_chunks = 1, # to parallelise Daymont QC
+    working_directory = tempdir()
+  )
+
+  expect_equal(
+    sort(basename(result_lmm)),
+    sort(c("female.zip", "male.zip"))
+  )
+
+  output_path <- file.path(tempdir(), "eggla")
+  dir.create(output_path, showWarnings = FALSE)
+
+  result_gwas <- run_eggla_gwas(
+    data = bmigrowth[
+      j = sex := c("0" = "f", "1" = "m")[as.character(sex)],
+    ],
+    results = result_lmm,
+    id_column = "ID",
+    traits = c("slope_.*", "auc_.*"),
+    covariates = c("sex", sprintf("PC%02d", 1:5)),
+    vcfs = list.files(
+      path = system.file("vcf", package = "eggla"),
+      pattern = "\\.vcf$|\\.vcf.gz$",
+      full.names = TRUE
+    ),
+    vep = NULL,
+    path = output_path,
+    bin_path = list(
+      bcftools = "/usr/bin/bcftools",
+      plink2 = system.file("bin", "plink2", package = "eggla")
+    ),
+    threads = 4
+  )
+  unlink(output_path, recursive = TRUE)
+  expect_equal(
+    sort(basename(result_gwas)),
+    sort(c(
+      "slope_12--17.glm.linear-gwas.csv.gz", "auc_0--0.5.glm.linear-gwas.csv.gz",
+      "auc_1.5--3.5.glm.linear-gwas.csv.gz", "slope_1.5--3.5.glm.linear-gwas.csv.gz",
+      "auc_6.5--10.glm.linear-gwas.csv.gz", "auc_12--17.glm.linear-gwas.csv.gz",
+      "slope_0--0.5.glm.linear-gwas.csv.gz", "slope_6.5--10.glm.linear-gwas.csv.gz",
+      "male.zip", "female.zip", "gwas-software.txt"
+    ))
+  )
+})
