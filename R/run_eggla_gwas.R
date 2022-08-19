723a2b1e42b0fda5e265676355b62c80eb63901f
Mickaël Canouil <mcanouil@users.noreply.github.com>
2022-08-19 14:14:22 +0200

    fix: ensure data are data.table

--- R/run_eggla_gwas.R
+++ R/run_eggla_gwas.R
@@ -220,14 +220,14 @@ run_eggla_gwas <- function(
 
   if (!inherits(data, "data.frame")) data <- data.table::fread(data)
   dt <- data.table::merge.data.table(
-    x = data[
+    x =  data.table::data.table(data)[
       j = (id_column) := lapply(.SD, as.character),
       .SDcols = c(id_column)
     ][
       j = data.table::first(.SD),
       by = c(id_column)
     ],
-    y = derived_parameters_dt[
+    y =  data.table::data.table(derived_parameters_dt)[
       j = (id_column) := lapply(.SD, as.character),
       .SDcols = c(id_column)
     ],
