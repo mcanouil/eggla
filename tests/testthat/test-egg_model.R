cad86219e12a6752c2f3d84fb7e71809205b30f5
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2024-11-06 10:59:14 +0000

    test: make the snapshots more robust

--- tests/testthat/test-egg_model.R
+++ tests/testthat/test-egg_model.R
@@ -14,15 +14,31 @@ test_that("egg_model", {
     )
   )
 
-  expect_snapshot(head(egg_correlations(fit = res2), 10))
-  expect_snapshot(head(egg_aucs(fit = res2), 10))
-  expect_snapshot(head(egg_slopes(fit = res2), 10))
+  expect_snapshot(egg_correlations(fit = res2)[, lapply(.SD, round, digits = 2L), .SDcols = is.numeric])
+  expect_snapshot({
+    out <- egg_aucs(fit = res2)
+    out[, names(out)[-1L]] <- round(out[, names(out)[-1L]], digits = 2L)
+    out
+  })
+  expect_snapshot({
+    out <- egg_slopes(fit = res2)
+    out[, names(out)[-1L]] <- round(out[, names(out)[-1L]], digits = 2L)
+    out
+  })
 
   for (s in c("predicted", "observed")) {
-    expect_snapshot(head(compute_apar(fit = res2, from = s)[AP | AR], 10))
+    expect_snapshot({
+      out <- compute_apar(fit = res2, from = s)[AP | AR]
+      out[, c(2L, 3L)] <- round(out[, c(2L, 3L)], digits = 2L)
+      out
+    })
   }
 
-  expect_snapshot(head(egg_outliers(fit = res2), 10))
+  expect_snapshot({
+    out <- egg_outliers(fit = res2)
+    out[, "Distance_IQR"] <- round(out[, "Distance_IQR"], digits = 2L)
+    out
+  })
 
   set.seed(1234)
   dta <- bmigrowth[bmigrowth[["sex"]] == 0, ]
@@ -36,16 +52,16 @@ test_that("egg_model", {
 
   expect_warning(predict_bmi(res)[order(egg_id, egg_ageyears)])
 
-  expect_snapshot(
-    head(
-      predict_bmi(
-        fit = res,
-        filter = "source == 'A'"
-      )[
-        order(egg_id, egg_ageyears),
-        egg_bmi := round(egg_bmi, digits = 2)
-      ],
-      10
-    )
-  )
+  expect_snapshot({
+    out <- predict_bmi(
+      fit = res,
+      filter = "source == 'A'"
+    )[
+      order(egg_id, egg_ageyears),
+      egg_bmi := round(egg_bmi, digits = 2)
+    ]
+    out <- head(out, 50)
+    out[, c(3L, 4L)] <- round(out[, c(3L, 4L)], digits = 2L)
+    out
+  })
 })
